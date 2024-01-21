f6ddc014b78217ff90458b7a8b20cd1b0816247b
hikaricp <dockerc@163.com>
2024-01-21 22:51:34 +0800

    position不能大于limit限制

---
+++ src/cn/eaay/ch1/buffer/Test2_1.java
@@ -0,0 +1,16 @@
+package cn.eaay.ch1.buffer;
+
+import java.nio.ByteBuffer;
+
+public class Test2_1 {
+    public static void main(String[] args) {
+        byte[] byteArray = new byte[]{1, 2, 3};
+        ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
+        byteBuffer.limit(2);
+        try {
+            byteBuffer.position(3);
+        } catch (IllegalArgumentException e) {
+            System.out.println("ByteBuffer 的 position 位置不能大于其 limit 限制");
+        }
+    }
+}
